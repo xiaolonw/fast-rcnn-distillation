5c98207be9e2397de5796caf0f1f77ca5bdd3728
xiaolonw <xiaolonw@cs.cmu.edu>
2016-02-17 17:26:00 -0500

    large

--- scripts/test_scratch.sh
+++ scripts/test_scratch.sh
@@ -5,6 +5,6 @@ test_set='test'
 modality="image_norm"
   PYTHONPATH='.' PYTHONUNBUFFERED="True" python tools/test_net.py --gpu 1 \
     --def scripts/scratch/test.prototxt \
-    --net /nfs.yoda/xiaolonw/fast_rcnn/models/scratch/fast_rcnn_iter_40000.caffemodel \
+    --net /nfs.yoda/xiaolonw/fast_rcnn/models/scratch/fast_rcnn_iter_50000.caffemodel \
     --imdb nyud2_"$modality"_2015_"$test_set" \
     --cfg scripts/scratch/config.prototxt
